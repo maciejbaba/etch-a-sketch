2c2f8294a219400b7b117eeac777c669ff6555b0
maciek5000 <maciek2baba2@gmail.com>
2022-09-17 18:20:18 +0200

    Create grid, center, make space for menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.getElementById("container")
+
+function createGrid(rows, columns) {
+  container.style.setProperty("--grid-rows", rows)
+  container.style.setProperty("--grid-columns", columns)
+  for (c = 0; c < rows * columns; c++) {
+    let cell = document.createElement("div")
+    container.appendChild(cell).className = "grid-item"
+  }
+}
+
+createGrid(16, 16)
