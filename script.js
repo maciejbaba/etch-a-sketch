62d27ef2687a481e925d6a46fd722d47a55b293d
maciek5000 <maciek2baba2@gmail.com>
2022-09-18 20:39:41 +0200

    add button active color, add color picker, add rainbow mode, add eraser mode, add color mode, add clear functionality, fix issues

--- script.js
+++ script.js
@@ -1,4 +1,69 @@
 const container = document.getElementById("grid")
+createGrid(16, 16)
+
+const gridSquares = [...document.getElementsByClassName("grid-item")]
+
+const pickColorButton = document.getElementById("pick-color")
+const colorModeButton = document.getElementById("color-mode-button")
+const rainbowModeButton = document.getElementById("rainbow-mode-button")
+const eraserModeButton = document.getElementById("eraser-mode-button")
+const clearButton = document.getElementById("clear-button")
+const size = document.getElementById("size")
+
+let drawColor = pickColorButton.value
+let drawMode = "color"
+setModeButtonActive(drawMode)
+
+colorModeButton.addEventListener("click", function() {
+  drawMode = "color"
+  setModeButtonActive(drawMode)
+})
+
+rainbowModeButton.addEventListener("click", function() {
+  drawMode = "rainbow"
+  setModeButtonActive(drawMode)
+})
+
+eraserModeButton.addEventListener("click", function() {
+  drawMode = "eraser"
+  setModeButtonActive(drawMode)
+})
+
+clearButton.addEventListener("click", function() {
+  gridSquares.forEach(function(square) {
+    square.style.backgroundColor = "white"
+  })
+})
+
+gridSquares.forEach(function(square) {
+  square.addEventListener("mouseover", function() {
+    square.style.backgroundColor = setDrawColor(drawMode)
+  })
+})
+
+function setDrawColor(mode) {
+  if(mode === "color") return pickColorButton.value
+
+  if (mode === "rainbow") return pickRandomHexColor()
+
+  if (mode == "eraser") return "white"
+}
+
+function setModeButtonActive(mode) {
+  if (mode === "color") colorModeButton.style.backgroundColor = "aqua"
+
+  if (mode === "rainbow") rainbowModeButton.style.backgroundColor = "aqua"
+
+  if (mode === "eraser") eraserModeButton.style.backgroundColor = "aqua"
+}
+
+function pickRandomHexColor() {
+  const redHex = Math.floor(Math.random() * 256).toString(16)
+  const greenHex = Math.floor(Math.random() * 256).toString(16)
+  const blueHex= Math.floor(Math.random() * 256).toString(16)
+
+  return "#" + redHex + greenHex + blueHex
+}
 
 function createGrid(rows, columns) {
   container.style.setProperty("--grid-rows", rows)
@@ -8,5 +73,3 @@ function createGrid(rows, columns) {
     container.appendChild(cell).className = "grid-item"
   }
 }
-
-createGrid(16, 16)
